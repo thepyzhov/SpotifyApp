327af493186fc3d79477099d65cb10eec1c1b688
Dmitry Pyzhov <fiche40@gmail.com>
2022-04-14 18:28:48 +0400

    Initial Commit

---
+++ SpotifyApp/Controllers/Core/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by Dmitry Pyzhov on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+}
+
