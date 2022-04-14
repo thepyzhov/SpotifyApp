9db811b8e90f22c84ed09a7c12098764a0d30887
Dmitry Pyzhov <fiche40@gmail.com>
2022-04-14 19:57:14 +0400

    Set up TabBarViewController

--- SpotifyApp/Controllers/Core/TabBarViewController.swift
+++ SpotifyApp/Controllers/Core/TabBarViewController.swift
@@ -12,18 +12,31 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let homeViewController = HomeViewController()
+        let searchViewController = SearchViewController()
+        let libraryViewController = LibraryViewController()
+        
+        homeViewController.title = "Browse"
+        searchViewController.title = "Search"
+        libraryViewController.title = "Library"
+        
+        homeViewController.navigationItem.largeTitleDisplayMode = .always
+        searchViewController.navigationItem.largeTitleDisplayMode = .always
+        libraryViewController.navigationItem.largeTitleDisplayMode = .always
+        
+        let homeNavigationViewController = UINavigationController(rootViewController: homeViewController)
+        let searchNavigationViewController = UINavigationController(rootViewController: searchViewController)
+        let libraryNavigationViewController = UINavigationController(rootViewController: libraryViewController)
+        
+        homeNavigationViewController.navigationBar.prefersLargeTitles = true
+        searchNavigationViewController.navigationBar.prefersLargeTitles = true
+        libraryNavigationViewController.navigationBar.prefersLargeTitles = true
+        
+        homeNavigationViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        searchNavigationViewController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        libraryNavigationViewController.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "books.vertical"), tag: 1)
+        
+        setViewControllers([homeNavigationViewController, searchNavigationViewController, libraryNavigationViewController], animated: false)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
